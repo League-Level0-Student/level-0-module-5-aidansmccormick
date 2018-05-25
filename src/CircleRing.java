bc5c6260067ce9844cb62b25a526b6aac738da54
aidansmccormick <league@WTS22.attlocal.net>
2018-05-24 19:58:38 -0700

    Brain Machine Broke Pls input coolent

--- src/CircleRing.java
+++ src/CircleRing.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -6,29 +7,44 @@
 import org.jointheleague.graphical.robot.Robot;
 
 public class CircleRing {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        // 1. Make a new Robot
+		// 1. Make a new Robot
+		Robot juk = new Robot();
 
+		// 2. Set your robot’s position to x=150 and y=200
+		juk.setX(150);
+		juk.setY(200);
+		// 3. Put the robot's pen down
+		juk.penDown();
+		juk.setPenColor(255, 0, 0);
+		juk.hide();
+		// 4. Set the robot’s speed to 10
+		juk.setSpeed(10);
+		// 5. Do everything below here 360 times (use i as the counter)
+		for (int i = 0; i < 180; i++) {
+			juk.microTurn(1);
+			juk.move(20);
 
-       //  2. Set your robot’s position to x=150 and y=200
+			// 6. Move the robot 3 pixels
 
-        // 3. Put the robot's pen down
+			// 7. Turn the robot 1 degree
 
-        // 4. Set the robot’s speed to 10
+			// 8. If the counter i is divisible by 20 (hint: use mod operator %)..
 
-        // 5. Do everything below here 360 times (use i as the counter)
+			for (int j = 0; j < 180; j++) {
+				if (i % 20 == 0) {
 
-	        // 6. Move the robot 3 pixels
+					juk.move(20);
+					juk.microTurn(1);
 
-       //  7. Turn the robot 1 degree
-
-        // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
-
+				}
+			}
+		}
 		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
 
-				// 10. Move the robot 1 pixel
+		// 10. Move the robot 1 pixel
 
-				// 11. Turn the robot 1 degree
-}
+		// 11. Turn the robot 1 degree
+	}
 }
--- src/EvilPolygonsChallenge.java
+++ src/EvilPolygonsChallenge.java
@@ -1,28 +1,48 @@
-import java.awt.Color;
-
 import javax.swing.JOptionPane;
 
 import org.jointheleague.graphical.robot.Robot;
 
 public class EvilPolygonsChallenge {
-	
+
 	public static void main(String[] args) {
-		
-		//1. Create a new Robot
 
-		//2. Set the speed to 100
+		// 1. Create a new Robot
+		Robot juk = new Robot();
+		// 2. Set the speed to 100
+		juk.setSpeed(100);
+		int colorChoice = JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE,
+				JOptionPane.DEFAULT_OPTION, null, new String[] { "Color 1", "Color 2", "Color 3" }, 0);
 
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
-		
-		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
-		//4. Ask the use how many polygons they want to be drawn.
-		
-		//5. Use the robot to draw the number of polygons the user requested.
-		
-		//6. Make it so your shapes do not overlap
+		// 3. Use if statements to check the the value of colorChoice and set the pen
+		// color accordingly
+		if (colorChoice == 2)
+			;
+		{
+			juk.setPenColor(255, 0, 0);
+		}
+		if (colorChoice == 1)
+			;
+		{
+			juk.setPenColor(0, 0, 255);
+		}
+		if (colorChoice == 3)
+			;
+		{
+			juk.setPenColor(0, 255, 0);
+		}
+		// 4. Ask the use how many polygons they want to be drawn.
+		String turd = JOptionPane.showInputDialog("how many sided polygon would you like?");
 
-		//7. Challenge: add more colors to the Option Dialog.
+		int tuf = Integer.parseInt(turd);
+
+		if (tuf <= 2) {
+			JOptionPane.showMessageDialog(null, "*ERROR COMPUTE MACHINE BROKE*");
+
+		}
+		// 5. Use the robot to draw the number of polygons the user requested.
+
+		// 6. Make it so your shapes do not overlap
+
+		// 7. Challenge: add more colors to the Option Dialog.
 	}
 }
-
